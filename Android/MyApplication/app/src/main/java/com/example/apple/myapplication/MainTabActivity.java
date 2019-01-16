316dcd09dfae0aa0a14667c5ed147c88e84e8fbd
apple <839864600@qq.com>
2019-01-16 18:54:15 +0800

    android

---
+++ Android/MyApplication/app/src/main/java/com/example/apple/myapplication/MainTabActivity.java
@@ -0,0 +1,14 @@
+package com.example.apple.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainTabActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_tab);
+
+    }
+}
