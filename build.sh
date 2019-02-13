2d2eed4b408e070200e45c05c86c3bac7d9e9ce5
apple <839864600@qq.com>
2019-02-13 10:06:36 +0800

    k

--- build.sh
+++ build.sh
@@ -1,14 +1,22 @@
-basepath=$(cd `dirname $0`; pwd)
+basepath=$(pwd)
+
+echo "basepath: $basepath"
 
 if [ ! -f "/Users/apple/Library/j2objc-2.3/j2objc" ]; 
 then 
 echo "J2OBJC_HOME is not correctly defined, currently set to '${J2OBJC_HOME}'"; 
 exit 1; 
 fi;
-inputpath="/Users/apple/MyClientRemote/j2objc/CocoaPodsTest/CocoaPodsTest/JavaSource/src/Hello.java"
+inputpath="${basepath}/CocoaPodsTest/CocoaPodsTest/JavaSource/src/Hello.java"
+outputpat="${basepath}/CocoaPodsTest/ResultSource/"
 # INPUT_FILE_BASE="Hello"
 echo ${INPUT_FILE_PATH}
-"/Users/apple/Library/j2objc-2.3/j2objc" -d $basepath/CocoaPodsTest/ResultSource -sourcepath "${basepath}/CocoaPodsTest/JavaSource" --no-package-directories -use-arc --prefix com.example.mango.*=MM -g ${inputpath} -d ${basepath}/CocoaPodsTest/ResultSource/${INPUT_FILE_BASE}.h ${basepath}/CocoaPodsTest/ResultSource/${INPUT_FILE_BASE}.m;
+"/Users/apple/Library/j2objc-2.3/j2objc" \
+-classpath "${basepath}/CocoaPodsTest/CocoaPodsTest/JavaSource/src/Hello.class" \
+-sourcepath "$inputpath" \
+-d "$outputpat" \
+--no-package-directories -use-arc --prefix com.example.mango.*=MM \
+-g ${inputpath} \
 #${basepath}/CocoaPodsTest/ResultSource/${INPUT_FILE_BASE}.h
 #${basepath}/CocoaPodsTest/ResultSource/${INPUT_FILE_BASE}.m
-echo "finished!!!"
+echo "finished!!!==$?"
---
+++ build01.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+/Users/apple/Library/j2objc-2.3/j2objc \
+-d /Users/apple/MyClientRemote/j2objc/CocoaPodsTest/CocoaPodsTest/ResultSource \
+-sourcepath /Users/apple/MyClientRemote/j2objc/CocoaPodsTest/CocoaPodsTest/JavaSource/src/ \
+-use-arc \
+-g  \
+--prefix \
+--list
+
